0bdab794c8f67405d75aaa79c436801e71e930fe
kshitiz-008 <142625808+kshitiz-008@users.noreply.github.com>
2024-02-03 21:37:16 +0545

    Update index.js

--- index.js
+++ index.js
@@ -4,26 +4,27 @@ const axios = require('axios');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.get('/dalle', async (req, res) => {
-  const { text_query } = req.query;
+app.get('/imagine', async (req, res) => {
+  const { prompt } = req.query;
 
   const options = {
     method: 'POST',
-    url: 'https://text-to-image-dalle.p.rapidapi.com/generate/photon',
+    url: 'https://fast-stable-diffusion-xl.p.rapidapi.com/generate',
     headers: {
       'content-type': 'application/json',
       'X-RapidAPI-Key': '3fa82b3121msh60993f970f09819p15c22cjsncc0b065b5f1c',
-      'X-RapidAPI-Host': 'text-to-image-dalle.p.rapidapi.com',
+      'X-RapidAPI-Host': 'fast-stable-diffusion-xl.p.rapidapi.com',
     },
     data: {
-      text_query: text_query,
+      prompt: prompt,
+      negativePrompt: 'example negativePrompt',
     },
   };
 
   try {
     const response = await axios.request(options);
-    const { image_url, message, status } = response.data;
-    res.json({ image_url, message, status });
+    const { ImageUrl } = response.data;
+    res.json({ imageUrl: ImageUrl });
   } catch (error) {
     console.error(error);
     res.status(500).json({ message: 'Internal Server Error' });
